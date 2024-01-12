7878ac13302d3532ab5e653d71061ec6be7c5c2c
RageNeko26 <rneko2006@gmail.com>
2024-01-12 15:00:23 +0700

    feat: Using add operation

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
-package main 
+package main
+
+import "dyno-books/helper"
+
+var user1  = helper.Data{
+  UserEmail: "john@mail.com",
+  Book: []helper.Book{
+    {
+      Title: "Attack Of Titan",
+      Author: "Hajime",
+    },
+    {
+      Title: "Evangelion",
+      Author: "Anon",
+    },
+  },
+}
+
+var user2  = helper.Data{
+  UserEmail: "neko@mail.com",
+  Book: []helper.Book{
+    {
+      Title: "Sword Art Online",
+      Author: "Budi",
+    },
+    {
+      Title: "Blend W",
+      Author: "Supri",
+    },
+    {
+      Title: "Dokter Stone",
+      Author: "Agus",
+    },
+  },
+}
 
 func main() {
-
+  // Add some data -> helper.AddNote(&user1)
+  // Add some data -> helper.AddNote(&user2)
 }
